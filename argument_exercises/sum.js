cf33c59148b31d1e6b9f150c337c478ee4573154
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-09-11 18:39:27 -0400

    solve sum() using the ...rest operator

--- argument_exercises/sum.js
+++ argument_exercises/sum.js
@@ -10,3 +10,16 @@ function sum() {
 
 console.log(sum(1, 2, 3, 4) === 10);
 console.log(sum(1, 2, 3, 4, 5) === 15);
+
+function sum(...args) {
+    let sum = 0;
+
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+
+    return sum;
+}
+
+console.log(sum(1, 2, 3, 4) === 10);
+console.log(sum(1, 2, 3, 4, 5) === 15);
