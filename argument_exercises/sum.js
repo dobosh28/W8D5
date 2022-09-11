604d40294868edc4313cead428e612f736e38ab7
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-09-11 18:36:44 -0400

    solve sum() using the arguments keyword

--- argument_exercises/sum.js
+++ argument_exercises/sum.js
@@ -1,3 +1,12 @@
-function sum(args) {
-    
+function sum() {
+    let sum = 0;
+
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+
+    return sum;
 }
+
+console.log(sum(1, 2, 3, 4) === 10);
+console.log(sum(1, 2, 3, 4, 5) === 15);
